130e2d2d7faf71826d6ffb4f8001dd838bc506d4
Mykola Ursta <mykola.ursta@icloud.com>
2021-10-12 08:38:12 +0300

    fix rada

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
 #server '194.44.109.25', user: 'deploy', roles: %w{web app db}
-server '194.44.30.38', user: 'deploy', roles: %w{web app db}
+server '194.44.30.37', user: 'deploy', roles: %w{web app db}
